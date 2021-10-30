b30273b6d748c90cca0a31e93e2b8d582186ac2b
realappie <abdel@springtree.nl>
2021-10-31 00:14:54 +0200

    fix(icons-completion): add d flag

--- src/icons-completion-provider.ts
+++ src/icons-completion-provider.ts
@@ -29,7 +29,7 @@ export class IonIconsCompletionProvider implements CompletionItemProvider {
     constructor(private readonly globalStorageUriPath: string) {}
 
     async provideCompletionItems(document: TextDocument, position: Position) {
-        const ionIconNameRegex = new RegExp(/<ion-icon name=["|']([\w- ]*)/);
+        const ionIconNameRegex = new RegExp(/<ion-icon name=["|']([\w- ]*)/, 'd');
 
         const htmlContent = document.lineAt(position).text
 
